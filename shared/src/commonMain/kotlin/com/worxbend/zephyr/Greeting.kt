ddf1e0a2437bdb4976b8a6a3933e14502abe3bb9
w0rxbend <balyszyn@gmail.com>
2026-05-19 03:36:14 +0300

    Initial commit

---
+++ shared/src/commonMain/kotlin/com/worxbend/zephyr/Greeting.kt
@@ -0,0 +1,9 @@
+package com.worxbend.zephyr
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return sayHello(platform.name)
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/worxbend/zephyr/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.worxbend.zephyr
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
